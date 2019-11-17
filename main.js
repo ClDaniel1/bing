2d110302f643d0b0986a4f35c36248b4a0ada448
chenlong <chenlong1530@fzjxwl666.onaliyun.com>
2019-11-17 16:29:11 +0800

    init

---
+++ main.js
@@ -0,0 +1,132 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow, Menu, ipcMain, Tray, dialog} = require('electron')
+const path = require('path');
+const shell = require('electron').shell;
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+let tray
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1066,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    frame: false,
+    resizable: false
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // 当我们点击关闭时触发close事件，我们按照之前的思路在关闭时，隐藏窗口，隐藏任务栏窗口
+  // event.preventDefault(); 禁止关闭行为(非常必要，因为我们并不是想要关闭窗口，所以需要禁止默认行为)
+  // mainWindow.on('show', () => {
+  //   tray.setHighlightMode('always')
+  // })
+  // mainWindow.on('hide', () => {
+  //   tray.setHighlightMode('never')
+  // })
+  //创建系统通知区菜单
+  tray = new Tray(path.join(__dirname, 'icon.ico'));
+  const contextMenu = Menu.buildFromTemplate([
+    {label: '退出', click: () => {mainWindow.destroy()}},//我们需要在这里有一个真正的退出（这里直接强制退出）
+  ])
+  tray.setToolTip('Bing Wallpaper')
+  tray.setContextMenu(contextMenu)
+  tray.on('click', ()=>{ //我们这里模拟桌面程序点击通知区图标实现打开关闭应用的功能
+    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+    mainWindow.isVisible() ? mainWindow.setSkipTaskbar(false) : mainWindow.setSkipTaskbar(true);
+  })
+
+  let argv = process.argv
+  for(var i = 0; i < argv.length; i++) {
+    if(argv[i] == 'autoOpen') {
+      mainWindow.hide()
+    }
+  }
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+  createWindow()
+ 
+  Menu.setApplicationMenu(null)
+  //mainWindow.webContents.openDevTools();
+
+})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+ipcMain.on('close', e=> mainWindow.close());
+ipcMain.on('min', e=> mainWindow.hide());
+ipcMain.on('getAppPath', e=> e.returnValue = app.getPath('userData'));
+ipcMain.on('openAutoOpen', function() {
+  app.setLoginItemSettings({
+    openAtLogin: true,
+    args: [
+      'autoOpen'
+    ]
+  })
+})
+ipcMain.on('closeAutoOpen', function() {
+  app.setLoginItemSettings({
+    openAtLogin: false
+  })
+})
+ipcMain.on('choseFilePath', function(e) {
+  var filePath = dialog.showOpenDialogSync(mainWindow, {
+    properties: ['openFile', 'openDirectory']
+  })
+  e.returnValue = filePath
+})
+
+ipcMain.on('getVersion', function(e) {
+  e.returnValue = app.getVersion()
+})
+
+ipcMain.on('needUpdate', function(e, data) {
+  let res = dialog.showMessageBoxSync(mainWindow, {
+     'type' : 'info',
+     'buttons' : ['取消', '确定'],
+     'title' : '有新版本',
+     'message' : '旧版本：' + app.getVersion() + '\n新版本：' + data.version + "\n" + data.note
+  })
+  if (res == 1) {
+    shell.openExternal(data.url)
+  }
+})
---
+++ renderer.js
@@ -0,0 +1,337 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+const {ipcRenderer: ipc, remote } = require('electron');
+
+$("#closeWin").click(() => {
+    if (config.min == 1) {
+        ipc.send('min');
+    } else {
+        ipc.send('close');
+    }
+})
+
+let config = getConfig();
+let idx = 0;
+let date = 0;
+let timerSet = '';
+let timerDownDay = '';
+let startDate = new Date().getDate();
+let isOnline = 1;
+initConfig();
+
+function getConfigPath() {
+    return remote.app.getAppPath() + '/config.json'
+}
+
+function getConfig() {
+    let fs=require("fs");
+    let configPath = getConfigPath();
+    let exists = fs.existsSync(configPath)
+    if (!exists) {
+        let config = {
+            'autoOpen' : '0',
+            'autoSet' : '0',
+            'autoDown' : '0',
+            'min' : '0',
+            'savePath' : 'c:/bingWallpaper/',
+        };
+        saveConfig(config);
+        return config;
+    } else {
+        let config = JSON.parse(fs.readFileSync(configPath));
+        return config;
+    }
+}
+
+function saveConfig(config) {
+    let fs = require("fs");
+    let configPath = getConfigPath();
+    let jsonObj = JSON.stringify(config);
+    fs.writeFile(configPath, jsonObj, function (err) {
+        if(err){
+            remote.dialog.showErrorBox('修改配置失败！', err)
+            console.log(err);
+        }else{
+            console.log("file success！！！")
+        }
+	})
+}
+
+function initConfig() {
+    if (config.autoOpen == 1) {
+        $(".autoOpen")[0].checked = true;
+    }
+    if (config.autoSet == 1) {
+        $(".autoSet")[0].checked = true;
+        clearAutoSet();
+        setAutoSet();
+        saveImage(1);
+    }
+    if (config.autoDown == 1) {
+        $(".autoDown")[0].checked = true;
+    }
+    if (config.min == 1) {
+        $(".min")[0].checked = true;
+    }
+    if (config.autoDownDay == 1) {
+        $(".autoDownDay")[0].checked = true;
+        clearAutoDownDay();
+        setAutoDownDay();
+    }
+    $("#savePath").html(config.savePath)
+}
+
+let showImg = 0;
+
+if (!navigator.onLine) {
+    $("#body > div").fadeOut(300);
+    $(".tips").stop().fadeIn(300);
+    isOnline = 0;
+} else {
+    getBingImg();
+}
+
+window.addEventListener('online',  function() {
+    isOnline = 1;
+    if (showImg == 0) {
+        getBingImg();
+    }
+})
+
+window.addEventListener('offline',  function() {
+    isOnline = 0;
+    if (showImg == 0) {
+        $("#body > div").fadeOut(300);
+        $(".tips").stop().fadeIn(300);
+    }
+})
+
+function getBingImg(type) {
+    if (!checkNetWork()) {
+        return ;
+    }
+    if (type == 'pre') {
+        idx ++;
+        if (idx > 7) {
+            idx = 7;
+            return alert('已达最后一天')
+        }
+    } else if (type == 'next') {
+        idx --;
+        if (idx < 0) {
+            idx = 0;
+            return alert('已达最新一天')
+        }
+    }
+    $("#body > div").fadeOut(300);
+    $(".loading").stop().fadeIn(300);
+    $.ajax({
+        url:'http://cn.bing.com/HPImageArchive.aspx?format=js&idx=' + idx + '&n=1',
+        success: function(ret) {
+            date = ret.images[0].enddate;
+            $("#imgMain").html('<img draggable="false" src="http://www.bing.com' + ret.images[0].url + '" />');
+            $("#copyright").html(ret.images[0].copyright)
+            $("#body > div").fadeOut(300);
+            $("#img").stop().fadeIn(300);
+            showImg = 1;
+            if (config.autoDown == 1) {
+                saveImage()
+            }
+        }
+    })
+}
+
+function getBingImgUrl(idx, callBack) {
+    $.ajax({
+        url:'http://cn.bing.com/HPImageArchive.aspx?format=js&idx=' + idx + '&n=1',
+        async: false,
+        success: function(ret) {
+            callBack('http://www.bing.com' + ret.images[0].url);
+        }
+    })
+}
+
+$('.leftBtn i').click(() => {
+    if (checkNetWork()) {
+        getBingImg('pre')
+    }
+})
+$('.rightBtn i').click(() => {
+    if (checkNetWork()) {
+        getBingImg('next')
+    }
+})
+
+$(".down").click(() => saveImage())
+$(".set").click(() => saveImage(1))
+
+function saveImage(setBg, imgUrl) {
+    if (!checkNetWork()) {
+        return ;
+    }
+    if (imgUrl == undefined) {
+        imgUrl = $("#imgMain img").attr('src');
+    }
+    let fs = require("fs");
+    if (!fs.existsSync(config.savePath)) {  
+        fs.mkdirSync(config.savePath);  
+    }  
+
+    var xhr = new XMLHttpRequest();    
+    xhr.open("get", imgUrl, true);
+    xhr.responseType = "blob";
+    xhr.onload = function() {
+        if (this.status == 200) {
+            var blob = this.response;
+            let reader = new FileReader();
+            reader.readAsDataURL(blob);
+            reader.onload = function(e) {
+                var dataBuffer = new Buffer(e.target.result.replace(/^data:image\/\w+;base64,/,""), 'base64');
+                let savePath = config.savePath + 'BingWallpaper_' + date + '.jpg';
+                fs.writeFile(savePath, dataBuffer, function (err) {
+                    if(err){
+                        remote.dialog.showErrorBox('保存文件出错了！', err)
+                        console.log(err);
+                    }else{
+                        if (setBg) {
+                            const wallpaper = require('wallpaper');
+                            wallpaper.set(savePath);
+                        }
+                    }
+                })
+            }
+        }    
+    }, xhr.send();
+}
+
+$("#settingBtn").click(() => {
+    if ($("#setting").hasClass('active')) {
+        $("#body").css({filter:"blur(0)", transform: "scale(1)"});
+    } else {
+        $("#body").css({filter:"blur(20px)", transform: "scale(1.1)"});
+    }
+    $("#setting").toggleClass('active').fadeToggle(300)
+})
+
+$(".choseFilePath").click(() => {
+    let filePath = ipc.sendSync('choseFilePath');
+    if (filePath != undefined) {
+        filePath = filePath[0];
+        config.savePath = filePath;
+        saveConfig(config);
+        initConfig()
+    }
+})
+
+$("#infoBtn").click(() => {
+    remote.shell.openExternal('https://www.ihawo.com/archives/106.html')
+})
+
+$("#setting input[type=checkBox]").change(function() {
+    var name = $(this).attr('name');
+    var checked = $(this).is(':checked')
+    switch(name) {
+        case 'autoOpen':
+            if (checked) {
+                ipc.send('openAutoOpen')
+            } else {
+                ipc.send('closeAutoOpen')
+            }
+            config.autoOpen = checked ? 1 : 0;
+            break;
+        case 'autoSet':
+            if (checked) {
+                setAutoSet()
+            } else {
+                clearAutoSet()
+            }
+            config.autoSet = checked ? 1 : 0;
+            break;
+        case 'autoDown':
+            config.autoDown = checked ? 1 : 0;
+            break;
+        case 'min':
+            config.min = checked ? 1 : 0;
+            break;
+        case 'autoDownDay':
+            if (checked) {
+                setAutoDownDay()
+            } else {
+                clearAutoDownDay()
+            }
+            config.autoDownDay = checked ? 1 : 0;
+            break;
+    }
+    saveConfig(config)
+})
+
+function setAutoSet() {
+    timerSet = setInterval(function() {
+        let nowDate = new Date().getDate();
+            if (nowDate != startDate) {
+            getBingImgUrl(0, function(url) {
+                saveImage(1, url);
+            });
+        }
+    }, 3600000)
+}
+
+function clearAutoSet() {
+    clearInterval(timerSet);
+}
+
+function setAutoDownDay() {
+    timerDownDay = setInterval(function() {
+        let nowDate = new Date().getDate();
+        if (nowDate != startDate) {
+            getBingImgUrl(0, function(url) {
+                saveImage(0, url);
+            });
+        }
+    }, 3600000)
+}
+
+function clearAutoDownDay() {
+    clearInterval(timerDownDay);
+}
+
+function checkUpdate() {
+    if (isOnline == 0) {
+        return ;
+    }
+    let version = ipc.sendSync('getVersion');
+    let versionArr = version.split('\.');
+    $.ajax({
+        url: 'https://www.ihawo.com/checkBingWallpaoerUpdate.php',
+        dataType: 'json',
+        success: function(ret) {
+            let newVersion = ret.version;
+            let newVersionArr = newVersion.split('\.');
+            let needUpdate = false;
+            if (newVersionArr[0] == versionArr[0]) {
+                if (newVersionArr[1] == versionArr[1]) {
+                    if (newVersionArr[2] > versionArr[2]) {
+                        needUpdate = true;
+                    }
+                } else if (newVersionArr[1] > versionArr[1]) {
+                    needUpdate = true;
+                }
+            } else if (newVersionArr[0] > versionArr[0]) {
+                needUpdate = true;
+            }
+            if (needUpdate) {
+                ipc.send('needUpdate', ret)
+            }
+        }
+    })
+}
+checkUpdate();
+
+function checkNetWork() {
+    if(!isOnline) {
+        remote.dialog.showErrorBox('啊哦,当前无网络！', '');
+        return false;
+    }
+    return true;
+}
